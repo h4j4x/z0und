973629cd4a76f0524d55c65f110932ed29532351
h4j4x <miche.atucha@gmail.com>
2023-03-12 13:50:30 -0500

    google drive audio scan

--- lib/handler/impl/google_handler.dart
+++ lib/handler/impl/google_handler.dart
@@ -48,28 +48,38 @@ class GoogleHandler implements AudioMetaHandler {
     return Future.value(null);
   }
 
-  Future removeAuth() async {
-    final signed = await hasAccount;
-    if (signed) {
-      return googleSignIn.signOut();
-    }
-    return Future.value(null);
-  }
+  Future removeAuth() => googleSignIn.signOut();
 
   @override
   String get handlerId => _id;
 
   @override
-  Future<bool> get handlerIsEnabled => googleSignIn.isSignedIn();
+  Future<bool> get handlerIsEnabled async {
+    await googleSignIn.signInSilently();
+    return googleSignIn.isSignedIn();
+  }
 
   @override
   Future<List<AudioMeta>> listAudiosMetas() async {
-    // TODO: implement listAudiosMetas
+    await hasAccount;
     final httpClient = (await googleSignIn.authenticatedClient());
     if (httpClient != null) {
       try {
         final driveApi = DriveApi(httpClient);
-        driveApi.files.list();
+        final files = await driveApi.files.list(
+          corpora: 'user',
+          pageSize: 100,
+          q: 'mimeType contains \'audio/\'',
+        );
+        if (files.files != null) {
+          return files.files!
+              .where((file) => GoogleAudioMeta.canParse(file))
+              .map<AudioMeta>((file) => GoogleAudioMeta(
+                    name: file.name!,
+                    code: file.id!,
+                  ))
+              .toList();
+        }
       } catch (error) {
         debugPrint('GOOGLE driveApi error: $error');
       } finally {
@@ -80,8 +90,36 @@ class GoogleHandler implements AudioMetaHandler {
   }
 
   @override
-  Future<AudioSource?> fetchAudioSource(AudioMeta audioMeta) {
+  Future<AudioSource?> fetchAudioSource(AudioMeta audioMeta) async {
+    await hasAccount;
     // TODO: implement fetchAudioSource
     return Future.value(null);
   }
+}
+
+class GoogleAudioMeta implements AudioMeta {
+  static bool canParse(File file) => file.name != null && file.id != null;
+
+  @override
+  final String name;
+
+  @override
+  final String handlerId;
+
+  @override
+  final String code;
+
+  @override
+  String? audioName;
+
+  @override
+  int? durationInSeconds;
+
+  @override
+  bool get enabled => true;
+
+  GoogleAudioMeta({
+    required this.name,
+    required this.code,
+  }) : handlerId = GoogleHandler._id;
 }
--- lib/service/config.dart
+++ lib/service/config.dart
@@ -1,7 +1,9 @@
 import '../ioc.dart';
 
+/// App configuration service.
 abstract class ConfigService {
   factory ConfigService() => Ioc.get<ConfigService>();
 
+  /// Reads a config for given [key].
   String? read(String key);
 }
